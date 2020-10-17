d1e3149e4be300bb787d2aefe770b001d827409a
Markus Tran <thebluepillow@gmail.com>
2020-10-17 14:17:01 -0700

    init

---
+++ pages/project2.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Typography from '@material-ui/core/Typography'
+import Page from '../src/Page'
+import { $, $$ } from '../src/MathJax'
+
+const Project2 = () => {
+  return (
+    <Page>
+      <Typography variant='h5' paragraph>Problem 1</Typography>
+    </Page>
+  )
+}
+
+export default Project2
